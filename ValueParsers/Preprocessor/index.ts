69acf56625ff125d5002f24da05e606f5f3b6a36
KIBA <kjh030529@gmail.com>
2022-11-19 22:10:06 +0900

    Add Preprocessor pool initializer

--- ValueParsers/Preprocessor/index.ts
+++ ValueParsers/Preprocessor/index.ts
@@ -1,6 +1,29 @@
-import * as Kind from "../../peg/kind" 
+import * as Kind from "@/peg/kind" 
+import { isStyleStatement, isTransformStatement, isAnimationStatement } from "@/peg/traveler";
 
 interface PreprocessorContext
 {
-    pool : Map<string, Kind.Statement>;
+    dataPool : Map<string, Kind.Statement>;
+    parentTransform? : Kind.TransformStatement;
+}
+
+function InitializePool(source : Kind.SourceFile)
+{
+    const ctx : PreprocessorContext = {dataPool : new Map}
+
+    source.statements.forEach(statement => {
+        if(isStyleStatement(statement) || 
+            isTransformStatement(statement) || 
+            isAnimationStatement(statement))
+        {
+            ctx.dataPool.set(statement.name.text, statement);
+        }
+    });
+
+    return ctx;
+}
+
+function AdjustTransform(ctx: PreprocessorContext)
+{
+
 }
